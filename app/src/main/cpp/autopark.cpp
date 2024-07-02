6db4209fcd280bc2549b6c8a88f7903372002cea
Jingyan LOU <jlou0008@student.monash.edu>
2024-07-02 23:33:07 +1000

    final

---
+++ app/src/main/cpp/autopark.cpp
@@ -0,0 +1,16 @@
+//
+// Do not forget to dynamically load the C++ library into your application.
+//
+// For instance,
+//
+// In MainActivity.java:
+//    static {
+//       System.loadLibrary("autopark");
+//    }
+//
+// Or, in MainActivity.kt:
+//    companion object {
+//      init {
+//         System.loadLibrary("autopark")
+//      }
+//    }
