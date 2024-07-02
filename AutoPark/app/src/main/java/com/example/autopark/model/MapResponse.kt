2b5a039278b5631c63a323fdc5b8d035579203b6
Jingyan LOU <jlou0008@student.monash.edu>
2024-07-02 23:29:50 +1000

    final

--- AutoPark/app/src/main/java/com/example/autopark/model/MapResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.autopark.model
-
-data class MapResponse(
-    // the main whole JSON response
-    val makeLocation: List<MarkLocation> = listOf()
-)
--- AutoPark/app/src/main/java/com/example/autopark/viewmodel/NavigationViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.autopark.viewmodel
-
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
-class NavigationViewModel: ViewModel() {
-    val name: MutableState<String> = mutableStateOf("")
-
-}
